7562022183059f9661cebc22c1b52a0298cd051d
kourtney <kourtneylee1611@gmail.com>
2023-02-24 00:04:08 +0800

    1. Adding Docker image build command in Makefile 2. Use Makefile instead of script files to prevent confusion caused by path

--- {{cookiecutter.project_dir_name}}/scripts/docker-compose-build-restart.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-docker-compose stop && \
-    docker-compose rm -f && \
-    docker-compose build --no-cache && \
-    docker-compose up -d
--- {{cookiecutter.project_dir_name}}/scripts/test.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env sh
-env $(cat .env) poetry run pytest -s -vv
